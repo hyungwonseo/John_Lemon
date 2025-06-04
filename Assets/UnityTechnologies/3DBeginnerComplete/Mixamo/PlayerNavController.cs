03c556d1fb7a0390088f133ccbdfed8ec14a4a3d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-04 17:16:22 +0900

    update

--- Assets/UnityTechnologies/3DBeginnerComplete/Mixamo/PlayerNavController.cs
+++ Assets/UnityTechnologies/3DBeginnerComplete/Mixamo/PlayerNavController.cs
@@ -5,5 +5,42 @@ using UnityEngine.AI;
 
 public class PlayerNavController : MonoBehaviour
 {
-    
+    public float rotationSpeed = 10f;
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToClickPoint();
+        }
+
+        bool isWalking = agent.velocity.magnitude > 0.1f && !agent.isStopped;
+        animator.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            // È¸Àü
+            Quaternion lookRotation = Quaternion.LookRotation(agent.velocity.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation,
+                rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void MoveToClickPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
 }

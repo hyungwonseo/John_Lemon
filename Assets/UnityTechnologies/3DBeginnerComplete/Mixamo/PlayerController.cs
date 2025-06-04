e595df8887d9088a5ae4b9bdd3c767cc5aafb08b
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-04 14:15:08 +0900

    update

--- Assets/UnityTechnologies/3DBeginnerComplete/Mixamo/PlayerController.cs
+++ Assets/UnityTechnologies/3DBeginnerComplete/Mixamo/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     Animator animator;
 
     Vector3 movementInput;
+    string currentAnimation = "Idle";
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,8 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
         movementInput = new Vector3(moveX, 0, moveZ).normalized;
+
+        UpdateAnimations();
     }
 
     private void FixedUpdate()
@@ -36,5 +39,24 @@ public class PlayerController : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(movement);
             rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, Time.fixedDeltaTime * 10f);
         }
+    }
+
+    void UpdateAnimations()
+    {
+        if (movementInput.magnitude > 0)
+        {
+            PlayAnimation("Walk");
+        }else
+        {
+            PlayAnimation("Idle");
+        }
+    }
+
+    void PlayAnimation(string animationName)
+    {
+        if (currentAnimation == animationName) { return; }
+
+        animator.Play(animationName);
+        currentAnimation = animationName;
     }
 }

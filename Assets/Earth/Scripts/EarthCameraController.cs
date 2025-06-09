e6873d3b1c36c88289151d1eab115650935206e2
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-09 16:13:52 +0900

    update

--- Assets/Earth/Scripts/EarthCameraController.cs
+++ Assets/Earth/Scripts/EarthCameraController.cs
@@ -28,11 +28,28 @@ public class EarthCameraController : MonoBehaviour
 
     void HandleRotation()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+            float mouseY = -Input.GetAxis("Mouse Y") * rotationSpeed;
+            targetRotation.x += mouseY;
+            targetRotation.y += mouseX;
+            // x축(상하 회전)의 각도를 제한(카메라가 뒤집히지 않도록)
+            targetRotation.x = Mathf.Clamp(targetRotation.x, -90, 90);
+        }
+        currentRotation = Vector3.SmoothDamp(currentRotation, targetRotation, ref velocity, smoothTime);
+
+        Quaternion rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, 0);
+        transform.position = target.position - ((rotation * Vector3.forward) * currentZoom);
+        transform.LookAt(target.position);  
     }
 
     void HandleZoom()
     {
-       
+        float scroll = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        // 마우스휠을 위로 밀면 양수값이 리턴. 줌인하려면 빼야됨
+        // 마우스휠을 아래로 당기면 음수값이 리턴. 줌아웃하려면 더해야됨
+        currentZoom -= scroll;
+        currentZoom = Mathf.Clamp(currentZoom, minZoomDistance, maxZoomDistance);
     }
 }
